06b276543510a3dbd9cbb1e44d043673d5e41f6d
scrawach <scrawach@gmail.com>
2026-07-08 15:15:21 +0300

    feat: use 2 labels for text ticker

--- src/characters/ui/components/text_ticker.gd
+++ src/characters/ui/components/text_ticker.gd
@@ -6,13 +6,26 @@ extends Control
 		text = new_text
 		if not is_node_ready():
 			await ready
-		label.text = new_text
-	
-@export var speed: float = 50.0
+		label_1.text = new_text
+		label_2.text = new_text
+		
+		text_width = label_1.size.x
+		label_2.position.x = text_width + spacing
 
-@onready var label: Label = $Label
+@export var speed: float = 50.0
+@export var spacing: float = 10
+
+@onready var label_1: Label = $Label1
+@onready var label_2: Label = $Label2
+
+var text_width: float
 
 func _physics_process(delta: float) -> void:
-	label.position.x -= speed * delta
-	if label.position.x < -label.size.x:
-		label.position.x = size.x
+	label_1.position.x -= speed * delta
+	label_2.position.x -= speed * delta
+	
+	if -label_1.position.x > text_width:
+		label_1.position = label_2.position + Vector2(text_width + spacing, 0)
+	
+	if -label_2.position.x > text_width:
+		label_1.position = label_1.position + Vector2(text_width + spacing, 0)
